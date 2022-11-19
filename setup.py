39090038d6b55b8b0fc434a1d6d3fdd8548fd6ef
Dominick Meglio <dmeglio@gmail.com>
2022-11-19 17:37:38 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="waste_management",
-    version="1.1",
+    version="1.2",
     author="Dominick Meglio",
     license="MIT",
     author_email="dmeglio@gmail.com",
