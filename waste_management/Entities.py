f80eaea1b2b4ca44213014bae020ec5170e300a6
Dominick Meglio <dmeglio@gmail.com>
2022-11-19 17:36:33 -0500

    Added initial holiday parsing logic

--- waste_management/Entities.py
+++ waste_management/Entities.py
@@ -1,10 +1,21 @@
+import json
+
+
 class AccountInfo():
     def __init__(self, json):
         address = json["serviceAddress"]
         self.name = f'{address["street"]} {address["city"]} {address["state"]}'
         self.id = json["custAccountId"]
 
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
+
 class Service():
     def __init__(self, json):
         self.id = json["serviceId"]
         self.name = json["serviceDescription"]
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
--- waste_management/client.py
+++ waste_management/client.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import re
 import time
 import json
@@ -6,7 +6,7 @@ import jwt
 
 import requests
 
-from .const import API_KEY_AUTHENTICATION, API_KEY_CUSTOMER_SERVICES, API_KEY_USER_ACCOUNTS, REST_API_URL
+from .const import API_KEY_AUTHENTICATION, API_KEY_CUSTOMER_SERVICES, API_KEY_HOLIDAYS_USER_BY_ADDRESS, API_KEY_USER_ACCOUNTS, REST_API_URL
 from .Entities import AccountInfo, Service
 
 class WMClient:
@@ -24,6 +24,8 @@ class WMClient:
         self._okta_access_token = None
         self._client_id = None
         self._issuer = None
+        self._holiday_regex = re.compile("(\d{1,2}/\d{1,2}(?:/\d{2,4})?)")
+        self._delay_regex = re.compile("(\d+)(?: day)? delay")
 
     def _string_escape(self, input : str, encoding='utf-8'):
         return input.encode('latin1').decode('unicode-escape').encode('latin1').decode(encoding)
@@ -84,17 +86,42 @@ class WMClient:
     def get_service_pickup(self, account_id, service_id):
         self._apiKey = API_KEY_CUSTOMER_SERVICES
 
-        response = requests.get(REST_API_URL + f"account/{account_id}/service/{service_id}/pickupinfo", headers=self.headers, params={"lang": "en_US", "checkAlerts":"Y", "userId": self._user_id})
+        response = requests.get(REST_API_URL + f"account/{account_id}/service/{service_id}/pickupinfo", headers=self.headers, params={"lang": "en_US", "checkAlerts": "Y", "userId": self._user_id})
         response.raise_for_status()
 
         jsonData = json.loads(response.content.decode("UTF-8"))
 
+        upcoming_holiday_date = self.__get_holiday_delay_date(jsonData)
+        holiday_info = None
+        if upcoming_holiday_date is not None:
+            holiday_info = self.get_holidays(account_id)
+
         pickupDates = []
         for dateStr in jsonData["pickupScheduleInfo"]["pickupDates"]:
             date = datetime.strptime(dateStr, "%m-%d-%Y")
+            if date == upcoming_holiday_date:
+                date = holiday_info[date]
             pickupDates.append(date)
 
-        return pickupDates       
+        return pickupDates
+
+    def get_holidays(self, account_id):
+        self._apiKey = API_KEY_HOLIDAYS_USER_BY_ADDRESS
+
+        response = requests.get(REST_API_URL + f"user/{self._user_id}/account/{account_id}/holidays", headers=self.headers, params={"lang": "en_US", "type": "upcoming"})
+        response.raise_for_status()
+
+        jsonData = json.loads(response.content.decode("UTF-8"))
+
+        holidays = {}
+
+        if "holidayData" in jsonData:
+            for holiday in jsonData["holidayData"]:
+                holiday_message = holiday["holidayHours"]
+                holidays.update(self.__parse_holiday_impacted_dates(holiday_message))
+        return holidays
+                
+
 
     def api_get(self, path="", query=None):
         response = requests.get(REST_API_URL + path)
@@ -107,6 +134,63 @@ class WMClient:
 
         return json.loads(response.content.decode("UTF-8"))
 
+    def __get_holiday_delay_date(self, jsonData):
+        if "pickupDayInfo" in jsonData:
+            jsonNode = jsonData["pickupDayInfo"]
+            if "message" in jsonNode and jsonNode["message"] is not None:
+                # The only way to tell if the date is impacted by a holiday is via the message string
+                if "HOLIDAY" in jsonNode["message"].upper():
+                    date_str = jsonNode["date"]
+                    return datetime.strptime(date_str, "%m-%d-%Y")
+        return None
+
+    def __parse_holiday_impacted_dates(self, message):
+        """The API returns a text string that tells you about impacted dates. For example:
+        Due to the Thanksgiving Day holiday, your scheduled service will not be delayed 
+        except for 11/24, it will be on a 1 day delay excluding city of Allentown (no delay). 
+        11/25 service will be on 1 day delay except for city of Allentown will be serviced 
+        on the next pickup day.
+
+        So there is a lot to try to figure out here. First it tells us dates in the string that are impacted
+        in a pretty imprecise way, just a text string (no year) and tells us how many days to adjust. Second,
+        it tells us that certain cities are not impacted. This does not yet attempt to determine if you're in
+        an overriden city, but it does attempt to parse the text to determine if there is a delay.
+        """
+        impacted_dates = {}
+        # Look for anything that looks like a date, basically NN/NN and find them within the text.
+        for match in self._holiday_regex.finditer(message):
+            impacted_date_str = match.group(1)
+            month_day = datetime.strptime(impacted_date_str, "%m/%d")
+            impacted_date = month_day.replace(year=datetime.today().year)
+            if impacted_date < datetime.today():
+                impacted_date = month_day.replace(year=datetime.today().year+1)
+
+            impacted_dates[impacted_date] = match.span()
+
+        # Now that we have things that look like dates, we want to check the text after those dates
+        # to try to look for delays.
+        date_keys = list(impacted_dates.keys())
+        str_len = len(message)
+        for i in range(0,len(date_keys)):
+            date = date_keys[i]
+            current_date_span = impacted_dates[date]
+            (_,start) = current_date_span
+            if i < len(date_keys)-1:
+                (end,_) = impacted_dates[date_keys[i+1]]
+            else:
+                end = str_len
+            slice = message[start:end]
+
+            impacted_dates[date_keys[i]] = date_keys[i]
+            
+            if slice is not None:
+                delay_match = self._delay_regex.search(slice)
+                groups = delay_match.groups()
+                if len(groups) > 0:
+                    delay = groups[0]
+                    impacted_dates[date_keys[i]] = date_keys[i] + timedelta(days=int(delay))
+
+        return impacted_dates
 
     @property
     def headers(self):
